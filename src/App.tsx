ba2e39668348c70f0f44f8e29f0cc8fb7ff63866
Ronald Pereira <rpereira@taecontrol.com>
2023-02-18 00:11:45 -0400

    Lord Valdomero

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div className='w-96 h-96'>
+      <div className='flex flex-col justify-center space-y-3 py-2 px-4'>
+        <div>
+          <h2 className='text-lg font-bold'>Title</h2>
+          <span>MoonGuard</span>
+        </div>
+        <div>
+          <h2 className='text-lg font-bold'>Description</h2>
+          <span>Filadaputa</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
