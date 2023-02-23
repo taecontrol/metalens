c08d464fbee3b79549ab799a7ed3c40e98d13547
Ronald Pereira <rpereira@taecontrol.com>
2023-02-22 20:31:50 -0400

    [FEAT-2] background.js and content-script are compile on build --watch

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,18 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    watch: {
-      
+    outDir: 'dist',
+    watch: {},
+    rollupOptions: {
+      input: [
+        'src/background.js',
+        'src/content-script.js'
+      ],
+      output: {
+        entryFileNames: `js/[name].js`,
+        chunkFileNames: `js/[name].js`,
+        assetFileNames: `js/[name].[ext]`
+      },
     }
-  }
+  },
 })
