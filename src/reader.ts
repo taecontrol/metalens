a3cf81ccad1e5f8c4bbd237897c710f2c9938291
Ronald Pereira <rpereira@taecontrol.com>
2023-02-27 23:37:13 -0400

    Update reader.ts

--- src/reader.ts
+++ src/reader.ts
@@ -11,9 +11,9 @@ export default function getPageMetadata({ url }: getPageMetadataProps) {
 
   const NOT_FOUND = "Not found";
 
-  const ALL_META_TAGS = Array.from(document.querySelectorAll("meta"));
+  const META_TAGS = Array.from(document.querySelectorAll("meta"));
 
-  let siteMetadata: Metadata = {
+  const siteMetadata: Metadata = {
     twitter: {
       metadata: {
         title: NOT_FOUND,
@@ -39,12 +39,12 @@ export default function getPageMetadata({ url }: getPageMetadataProps) {
         image: NOT_FOUND_IMAGE_PATH,
         url: url,
       },
-      tags: ALL_META_TAGS,
+      tags: META_TAGS,
     },
     url,
   };
 
-  const includesAttribute = (tag: HTMLMetaElement, attribute: string) => {
+  const tagIncludesAttribute = (tag: HTMLMetaElement, attribute: string) => {
     return (
       tag.getAttribute("property")?.includes(attribute) ||
       tag.name.includes(attribute)
@@ -52,29 +52,29 @@ export default function getPageMetadata({ url }: getPageMetadataProps) {
   };
 
   const setMetadataAndTags = (standard: protocol) => {
-    ALL_META_TAGS.filter(
-      (tag) =>
-        tag.getAttribute("property")?.includes(standard) ||
-        tag.name.includes(standard)
+    META_TAGS.filter(
+      (tag) => tagIncludesAttribute(tag, standard)
     ).forEach((tag) => {
-      if (includesAttribute(tag, "title"))
+      if (tagIncludesAttribute(tag, "title"))
         siteMetadata[standard].metadata.title = tag.content;
 
-      if (includesAttribute(tag, "description"))
+      if (tagIncludesAttribute(tag, "description"))
         siteMetadata[standard].metadata.description = tag.content;
 
-      if (includesAttribute(tag, "image"))
+      if (tagIncludesAttribute(tag, "image"))
         siteMetadata[standard].metadata.image = tag.content;
 
-      if (includesAttribute(tag, "url"))
+      if (tagIncludesAttribute(tag, "url"))
         siteMetadata[standard].metadata.url = new URL(tag.content).hostname;
+
+      siteMetadata[standard].tags.push(Object.assign({}, tag));
     });
   };
 
   setMetadataAndTags("twitter");
   setMetadataAndTags("og");
 
-  ALL_META_TAGS.forEach((tag) => {
+  META_TAGS.forEach((tag) => {
     if (tag.name === "title") siteMetadata.general.metadata.title = tag.content;
     if (tag.name === "description")
       siteMetadata.general.metadata.description = tag.content;
